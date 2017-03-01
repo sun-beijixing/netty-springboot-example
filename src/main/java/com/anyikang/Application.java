6ddc187d3c794ed87ceac48166d8907d9602e125
wangwei <wangwei.0822@163.com>
2017-03-01 11:35:12 +0800

    添加配置文件

--- src/main/java/com/anyikang/Application.java
+++ src/main/java/com/anyikang/Application.java
@@ -1,30 +1,14 @@
 
 package com.anyikang;
 
-import com.anyikang.netty.ChannelRepository;
-import com.anyikang.netty.TCPServer;
-import com.anyikang.netty.handler.SomethingChannelInitializer;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import com.anyikang.netty.TCPServer;
 
 /**
  * Spring Java Configuration and Bootstrap
@@ -54,67 +38,5 @@ public class Application {
 
     }
 
-    @Value("${tcp.port}")
-    private int tcpPort;
-
-    @Value("${boss.thread.count}")
-    private int bossCount;
-
-    @Value("${worker.thread.count}")
-    private int workerCount;
-
-    @Value("${so.keepalive}")
-    private boolean keepAlive;
-
-    @Value("${so.backlog}")
-    private int backlog;
-
-    @SuppressWarnings("unchecked")
-    @Bean(name = "serverBootstrap")
-    public ServerBootstrap bootstrap() {
-        ServerBootstrap b = new ServerBootstrap();
-        b.group(bossGroup(), workerGroup())
-                .channel(NioServerSocketChannel.class)
-                .handler(new LoggingHandler(LogLevel.DEBUG))
-                .childHandler(somethingChannelInitializer);
-        Map<ChannelOption<?>, Object> tcpChannelOptions = tcpChannelOptions();
-        Set<ChannelOption<?>> keySet = tcpChannelOptions.keySet();
-        for (@SuppressWarnings("rawtypes") ChannelOption option : keySet) {
-            b.option(option, tcpChannelOptions.get(option));
-        }
-        return b;
-    }
-
-    @Autowired
-    @Qualifier("somethingChannelInitializer")
-    private SomethingChannelInitializer somethingChannelInitializer;
-
-    @Bean(name = "tcpChannelOptions")
-    public Map<ChannelOption<?>, Object> tcpChannelOptions() {
-        Map<ChannelOption<?>, Object> options = new HashMap<ChannelOption<?>, Object>();
-        options.put(ChannelOption.SO_KEEPALIVE, keepAlive);
-        options.put(ChannelOption.SO_BACKLOG, backlog);
-        return options;
-    }
-
-    @Bean(name = "bossGroup", destroyMethod = "shutdownGracefully")
-    public NioEventLoopGroup bossGroup() {
-        return new NioEventLoopGroup(bossCount);
-    }
-
-    @Bean(name = "workerGroup", destroyMethod = "shutdownGracefully")
-    public NioEventLoopGroup workerGroup() {
-        return new NioEventLoopGroup(workerCount);
-    }
-
-    @Bean(name = "tcpSocketAddress")
-    public InetSocketAddress tcpPort() {
-        return new InetSocketAddress(tcpPort);
-    }
-
-    @Bean(name = "channelRepository")
-    public ChannelRepository channelRepository() {
-        return new ChannelRepository();
-    }
 
 }
