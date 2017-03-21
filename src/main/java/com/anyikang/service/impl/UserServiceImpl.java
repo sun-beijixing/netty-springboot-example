188db26e3fea6b15e97ec05fa1538b44c62766ea
wangwei <wangwei.0822@163.com>
2017-03-21 13:52:39 +0800

    修改包

--- src/main/java/com/anyikang/service/impl/UserServiceImpl.java
+++ src/main/java/com/anyikang/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.anyikang.service.imp;
+package com.anyikang.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
