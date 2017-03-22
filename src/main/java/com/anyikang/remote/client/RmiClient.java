cc3fc4c64b310bffec7cabb4c39bac34595969c0
wangwei <wangwei.0822@163.com>
2017-03-22 14:46:31 +0800

    no message

--- src/main/java/com/anyikang/remote/client/RmiClient.java
+++ src/main/java/com/anyikang/remote/client/RmiClient.java
@@ -1,4 +1,4 @@
-package com.anyikang.remote.client;
+/*package com.anyikang.remote.client;
 
 import java.rmi.Naming;
 
@@ -7,9 +7,10 @@ import com.anyikang.remote.server.service.HelloService;
 
 public class RmiClient {
 	public static void main(String[] args) throws Exception {
-        String url = "rmi://localhost:1099/demo.zookeeper.rmi.server.HelloServiceImpl";
+        String url = "rmi://localhost:1099/com.anyikang.remote.server.impl.HelloServiceImpl";
         HelloService helloService = (HelloService) Naming.lookup(url);
         String result = helloService.sayHello("Jack");
         System.out.println(result);
     }
 }
+*/
--- src/main/java/com/anyikang/remote/server/RmiServer.java
+++ src/main/java/com/anyikang/remote/server/RmiServer.java
@@ -1,4 +1,4 @@
-package com.anyikang.remote.server;
+/*package com.anyikang.remote.server;
 
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
@@ -8,7 +8,7 @@ import com.anyikang.remote.server.impl.HelloServiceImpl;
 public class RmiServer {
 	public static void main(String[] args) throws Exception {
         int port = 1099;
-        String url = "rmi://localhost:1099/demo.zookeeper.rmi.server.HelloServiceImpl";
+        String url = "rmi://localhost:1099/com.anyikang.remote.server.impl.HelloServiceImpl";
         // JNDI 中创建注册表
         LocateRegistry.createRegistry(port);
         
@@ -17,3 +17,4 @@ public class RmiServer {
         System.out.println("服务器向命名表注册了个远程服务对象");
     }
 }
+*/
