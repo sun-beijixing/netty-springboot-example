262105f89c8031559b47adb97cf719a1b79d087b
wangwei <wangwei.0822@163.com>
2017-03-30 16:46:11 +0800

    验证开始和结束编码

--- src/main/java/com/anyikang/components/netty/coding/BytesToJsonDecode.java
+++ src/main/java/com/anyikang/components/netty/coding/BytesToJsonDecode.java
@@ -26,19 +26,17 @@ public class BytesToJsonDecode extends ByteToMessageDecoder {
 
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in,List<Object> out) throws Exception {
-		if(in.readableBytes()<20){//如果接受过来的十六进制串不是完整的串，则继续接受
+		if(in.readableBytes()<20){//最基本长度
 			return;
 		}
 		
 		in.markReaderIndex();
 		
-		byte beginCode = in.readByte();//起始位
+		byte beginCode = in.readByte();
 		//有可能是多条数据包，需要循环处理
 		if (beginCode==0x68){
 			String imeiCode=BCDUtils.byteToHexString(new byte[8],in);
-//			byte dataLength=in.readByte();//数据长度
-//			byte dataLength=in.readByte();//数据长度
-			int dataLength=BCDUtils.byteToInt(in.readByte());//数据长度
+			int dataLength=BCDUtils.byteToInt(in.readByte());
 			
 			if(in.readableBytes()<dataLength){
 				logger.debug("=============接收的数据长度小于规定的数据长度================");
@@ -46,20 +44,23 @@ public class BytesToJsonDecode extends ByteToMessageDecoder {
 				return;
 			}
 			
-			byte functionCode=in.readByte();//功能码
+			byte functionCode=in.readByte();
+			byte[] dataBody=new byte[dataLength-11-9];
+			in.readBytes(dataBody);
 			String time=BCDUtils.byteToHexString(new byte[7],in);
-			byte[] dataBody=null;
-			byte crc=in.readByte();//CRC8
-			byte endCode=in.readByte();//结束符
+			byte crc=in.readByte();
+			byte endCode=in.readByte();
 			
-			ByteToJsonBody messageBody = new ByteToJsonBody();
-			messageBody.setImeiCode(imeiCode);
-			messageBody.setDataLength(dataLength);
-			messageBody.setFunctionCode(functionCode);
-			messageBody.setDataBody(dataBody);
-			messageBody.setTime(time);
-			messageBody.setCrc(crc);
-			out.add(messageBody);
+			if(endCode==0x16){
+				ByteToJsonBody messageBody = new ByteToJsonBody();
+				messageBody.setImeiCode(imeiCode);
+				messageBody.setDataLength(dataLength);
+				messageBody.setFunctionCode(functionCode);
+				messageBody.setDataBody(dataBody);
+				messageBody.setTime(time);
+				messageBody.setCrc(crc);
+				out.add(messageBody);
+			}
 		}else{
 			messageError(ctx,"格式错误");
 		}
@@ -67,23 +68,23 @@ public class BytesToJsonDecode extends ByteToMessageDecoder {
 	
 	
 	private void messageError(final ChannelHandlerContext ctx,String returnMsg) {
-		String message = returnMsg; 
-		message = CommonUtils.encodeToBASE64(message);
-		byte[] header = new byte[4];
-		header[0]=(byte)1;
-		header[1]=(byte)1;
-		byte[] body = message.getBytes();
-		byte[] aaa = CommonUtils.shortToByteArray((short)body.length);
-		header[2] = aaa[0] ;
-		header[3] = aaa[1] ;
-		byte[] all = new byte[4+body.length];
-		System.arraycopy(header, 0, all, 0, 4);
-		System.arraycopy(body, 0, all, 4, body.length);
-
-		ByteBuf encoded = ctx.alloc().buffer(all.length);  
-		encoded.writeBytes(all);  
-		ctx.writeAndFlush(encoded);
-		ctx.channel().close();
+//		String message = returnMsg; 
+//		message = CommonUtils.encodeToBASE64(message);
+//		byte[] header = new byte[4];
+//		header[0]=(byte)1;
+//		header[1]=(byte)1;
+//		byte[] body = message.getBytes();
+//		byte[] aaa = CommonUtils.shortToByteArray((short)body.length);
+//		header[2] = aaa[0] ;
+//		header[3] = aaa[1] ;
+//		byte[] all = new byte[4+body.length];
+//		System.arraycopy(header, 0, all, 0, 4);
+//		System.arraycopy(body, 0, all, 4, body.length);
+//
+//		ByteBuf encoded = ctx.alloc().buffer(all.length);  
+//		encoded.writeBytes(all);  
+//		ctx.writeAndFlush(encoded);
+//		ctx.channel().close();
 		return;
 	}
 	
