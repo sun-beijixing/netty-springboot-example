7700577d5f1a58d63444927f6caf2803e5f99fbb
wangwei <wangwei.0822@163.com>
2017-04-06 15:43:18 +0800

    修改启动类

--- src/main/java/com/anyikang/Application.java
+++ src/main/java/com/anyikang/Application.java
@@ -4,17 +4,12 @@ package com.anyikang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.anyikang.components.rabbitmq.Receiver;
-import com.anyikang.components.rabbitmq.Sender;
-import com.anyikang.components.redis.RedisTest;
+//import com.anyikang.components.redis.RedisTest;
 
 /**
  * 
@@ -29,21 +24,6 @@ import com.anyikang.components.redis.RedisTest;
 public class Application {
 	
 	public static final Logger logger = LoggerFactory.getLogger(Application.class);  
-	
-	@Bean
-	public Sender mySender() {
-		return new Sender();
-	}
-	
-	@Bean
-	public Receiver myReceiver(){
-		return new Receiver();
-	}
-       
-	@Bean
-	public RedisTest myRedisTest(){
-		return new RedisTest();
-	}
 	
     public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class, args);
