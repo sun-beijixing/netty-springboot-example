b7680a5ddffcbda815bfb8c1a1f7c13e6e3fd9b0
wangwei <wangwei.0822@163.com>
2017-04-06 15:32:45 +0800

    添加设备监听器

---
+++ src/main/java/com/anyikang/components/rabbitmq/listener/DeviceListener.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.anyikang.components.rabbitmq.listener;
+
+import java.util.Date;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.anyikang.model.Device;
+import com.anyikang.service.DeviceService;
+
+/**
+ * @author wangwei
+ * @date 2017年4月6日
+ */
+@Component
+@RabbitListener(queues="device_queue")
+public class DeviceListener {
+	
+	@Autowired
+	private DeviceService deviceService;
+
+	@RabbitHandler
+	public void process(@Payload String xxx) {
+		System.out.println("=====EquipmentListener=========" + new Date() + ": " + xxx);
+		Device device=new Device();
+		device.setId(123);
+		deviceService.addDevice(device);
+		//调用service层业务逻辑，存储数据
+	}
+}
---
+++ src/main/java/com/anyikang/model/Device.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.anyikang.model;
+
+/**
+ * @author wangwei
+ * @date 2017年4月6日
+ */
+public class Device {
+
+	private long id;
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
