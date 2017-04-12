f4bb268d1e4b19da31566297c81946f38ebaccf0
wangwei <wangwei.0822@163.com>
2017-04-12 15:45:01 +0800

    废弃netty客户端测试类

--- src/main/java/client/TcpClient.java
+++ src/main/java/client/TcpClient.java
@@ -1,107 +1,107 @@
-/**
- * 
- */
-package client;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-import io.netty.util.CharsetUtil;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-  
-/**
- * @author wangwei
- * @date 2017年3月2日
- */
-public class TcpClient {  
-    private static final Logger logger = LoggerFactory.getLogger(TcpClient.class); 
-    public static String HOST = "127.0.0.1";  
-    public static int PORT = 8090;  
-      
-    public static Bootstrap bootstrap = getBootstrap();  
-    public static Channel channel = getChannel(HOST,PORT);  
-    
-	
-    /** 
-     * 初始化Bootstrap 
-     * @return 
-     */  
-    public static final Bootstrap getBootstrap(){  
-        EventLoopGroup group = new NioEventLoopGroup();  
-        Bootstrap b = new Bootstrap();  
-        b.group(group).channel(NioSocketChannel.class);  
-        b.handler(new ChannelInitializer<Channel>() {  
-            @Override  
-            protected void initChannel(Channel ch) throws Exception {  
-                ChannelPipeline pipeline = ch.pipeline();  
-                //添加字符串编码解码器
-                pipeline.addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));  
-                pipeline.addLast("encoder", new StringEncoder(CharsetUtil.UTF_8)); 
-                //添加POJO对象解码器 禁止缓存类加载器
-//                pipeline.addLast(new ObjectDecoder(1024,ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
-                //设置发送消息编码器
-//                pipeline.addLast(new ObjectEncoder());
-                //设置网络IO处理器
-                pipeline.addLast("handler", new TcpClientHandler());  
-            }  
-        });  
-        b.option(ChannelOption.SO_KEEPALIVE, true);  
-        return b;  
-    }  
-  
-    public static final Channel getChannel(String host,int port){  
-        Channel channel = null;  
-        try {  
-            channel = bootstrap.connect(host, port).sync().channel();  
-        } catch (Exception e) {  
-            logger.error(String.format("连接Server(IP[%s],PORT[%s])失败", host,port),e);  
-            return null;  
-        }  
-        return channel;  
-    }  
-  
-    public static void sendMsg(String msg) throws Exception {  
-        if(channel!=null){  
-            channel.writeAndFlush(msg).sync();  
-        }else{  
-            logger.warn("消息发送失败,连接尚未建立!");  
-        }  
-    }  
-    
-    public static void sendObject(Object obj) throws Exception {  
-    	if(channel!=null){  
-    		channel.writeAndFlush(obj).sync();  
-    	}else{  
-    		logger.warn("消息发送失败,连接尚未建立!");  
-    	}  
-    }  
-    
-  
-    public static void main(String[] args) throws Exception {  
-        try {  
-            long t0 = System.nanoTime();  
-            for (int i = 0; i < 10; i++) {  
-            	TcpClient.sendMsg(i+"你好1");  
-            	
-//            	ObjectRequest req=new ObjectRequest();
-//            	req.setId(i);
-//            	req.setName("张三"+i);
-//            	req.setAge(15+i);
-//            	TcpClient.sendObject(req);
-            }  
-            long t1 = System.nanoTime();  
-            System.out.println((t1-t0)/1000000.0);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-    }  
-}  
+///**
+// * 
+// */
+//package client;
+//
+//import io.netty.bootstrap.Bootstrap;
+//import io.netty.channel.Channel;
+//import io.netty.channel.ChannelInitializer;
+//import io.netty.channel.ChannelOption;
+//import io.netty.channel.ChannelPipeline;
+//import io.netty.channel.EventLoopGroup;
+//import io.netty.channel.nio.NioEventLoopGroup;
+//import io.netty.channel.socket.nio.NioSocketChannel;
+//import io.netty.handler.codec.string.StringDecoder;
+//import io.netty.handler.codec.string.StringEncoder;
+//import io.netty.util.CharsetUtil;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//  
+///**
+// * @author wangwei
+// * @date 2017年3月2日
+// */
+//public class TcpClient {  
+//    private static final Logger logger = LoggerFactory.getLogger(TcpClient.class); 
+//    public static String HOST = "127.0.0.1";  
+//    public static int PORT = 8090;  
+//      
+//    public static Bootstrap bootstrap = getBootstrap();  
+//    public static Channel channel = getChannel(HOST,PORT);  
+//    
+//	
+//    /** 
+//     * 初始化Bootstrap 
+//     * @return 
+//     */  
+//    public static final Bootstrap getBootstrap(){  
+//        EventLoopGroup group = new NioEventLoopGroup();  
+//        Bootstrap b = new Bootstrap();  
+//        b.group(group).channel(NioSocketChannel.class);  
+//        b.handler(new ChannelInitializer<Channel>() {  
+//            @Override  
+//            protected void initChannel(Channel ch) throws Exception {  
+//                ChannelPipeline pipeline = ch.pipeline();  
+//                //添加字符串编码解码器
+//                pipeline.addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));  
+//                pipeline.addLast("encoder", new StringEncoder(CharsetUtil.UTF_8)); 
+//                //添加POJO对象解码器 禁止缓存类加载器
+////                pipeline.addLast(new ObjectDecoder(1024,ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
+//                //设置发送消息编码器
+////                pipeline.addLast(new ObjectEncoder());
+//                //设置网络IO处理器
+//                pipeline.addLast("handler", new TcpClientHandler());  
+//            }  
+//        });  
+//        b.option(ChannelOption.SO_KEEPALIVE, true);  
+//        return b;  
+//    }  
+//  
+//    public static final Channel getChannel(String host,int port){  
+//        Channel channel = null;  
+//        try {  
+//            channel = bootstrap.connect(host, port).sync().channel();  
+//        } catch (Exception e) {  
+//            logger.error(String.format("连接Server(IP[%s],PORT[%s])失败", host,port),e);  
+//            return null;  
+//        }  
+//        return channel;  
+//    }  
+//  
+//    public static void sendMsg(String msg) throws Exception {  
+//        if(channel!=null){  
+//            channel.writeAndFlush(msg).sync();  
+//        }else{  
+//            logger.warn("消息发送失败,连接尚未建立!");  
+//        }  
+//    }  
+//    
+//    public static void sendObject(Object obj) throws Exception {  
+//    	if(channel!=null){  
+//    		channel.writeAndFlush(obj).sync();  
+//    	}else{  
+//    		logger.warn("消息发送失败,连接尚未建立!");  
+//    	}  
+//    }  
+//    
+//  
+//    public static void main(String[] args) throws Exception {  
+//        try {  
+//            long t0 = System.nanoTime();  
+//            for (int i = 0; i < 10; i++) {  
+//            	TcpClient.sendMsg(i+"你好1");  
+//            	
+////            	ObjectRequest req=new ObjectRequest();
+////            	req.setId(i);
+////            	req.setName("张三"+i);
+////            	req.setAge(15+i);
+////            	TcpClient.sendObject(req);
+//            }  
+//            long t1 = System.nanoTime();  
+//            System.out.println((t1-t0)/1000000.0);  
+//        } catch (Exception e) {  
+//            e.printStackTrace();  
+//        }  
+//    }  
+//}  
--- src/main/java/client/TcpClientHandler.java
+++ src/main/java/client/TcpClientHandler.java
@@ -1,60 +1,60 @@
-/**
- * 
- */
-package client;
-
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-  
-/**
- * @author wangwei
- * @date 2017年3月2日
- */
-public class TcpClientHandler extends ChannelInboundHandlerAdapter {  
-  
-    private static final Logger logger = LoggerFactory.getLogger(TcpClientHandler.class);
-
-	/* (non-Javadoc)
-	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelActive(io.netty.channel.ChannelHandlerContext)
-	 */
-	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.channelActive(ctx);
-	}
-
-	/* (non-Javadoc)
-	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelRead(io.netty.channel.ChannelHandlerContext, java.lang.Object)
-	 */
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg)
-			throws Exception {
-		// TODO Auto-generated method stub
-		super.channelRead(ctx, msg);
-		
-		if(msg instanceof String){
-			logger.info("=================String类型数据：");
-			logger.info((String)msg);
-			
-			if(msg.equals("ping")){//测试心跳
-				logger.info("----------------ok");
-				ctx.writeAndFlush("ok");
-			}
-		}
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelReadComplete(io.netty.channel.ChannelHandlerContext)
-	 */
-	@Override
-	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.channelReadComplete(ctx);
-	}
-  
-  
-}  
+///**
+// * 
+// */
+//package client;
+//
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.channel.ChannelInboundHandlerAdapter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//  
+///**
+// * @author wangwei
+// * @date 2017年3月2日
+// */
+//public class TcpClientHandler extends ChannelInboundHandlerAdapter {  
+//  
+//    private static final Logger logger = LoggerFactory.getLogger(TcpClientHandler.class);
+//
+//	/* (non-Javadoc)
+//	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelActive(io.netty.channel.ChannelHandlerContext)
+//	 */
+//	@Override
+//	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//		// TODO Auto-generated method stub
+//		super.channelActive(ctx);
+//	}
+//
+//	/* (non-Javadoc)
+//	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelRead(io.netty.channel.ChannelHandlerContext, java.lang.Object)
+//	 */
+//	@Override
+//	public void channelRead(ChannelHandlerContext ctx, Object msg)
+//			throws Exception {
+//		// TODO Auto-generated method stub
+//		super.channelRead(ctx, msg);
+//		
+//		if(msg instanceof String){
+//			logger.info("=================String类型数据：");
+//			logger.info((String)msg);
+//			
+//			if(msg.equals("ping")){//测试心跳
+//				logger.info("----------------ok");
+//				ctx.writeAndFlush("ok");
+//			}
+//		}
+//		
+//	}
+//
+//	/* (non-Javadoc)
+//	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelReadComplete(io.netty.channel.ChannelHandlerContext)
+//	 */
+//	@Override
+//	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+//		// TODO Auto-generated method stub
+//		super.channelReadComplete(ctx);
+//	}
+//  
+//  
+//}  
